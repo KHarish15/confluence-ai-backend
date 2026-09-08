8d60a5a3673a60df2d1fbb264f72082e658ad0fb
Harish <harishmaevish@gmail.com>
2026-09-08 21:38:48 +0530

    Configure frontend for Render deployment

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-
-  build: {
-    outDir: path.resolve(__dirname, "../../extension/app"),
-    emptyOutDir: true
-  }
+  base: "/"
 });
