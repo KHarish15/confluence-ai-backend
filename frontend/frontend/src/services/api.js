21685ed521e8b36f7a8dacf2ed2c727ece614c6c
Harish <harishmaevish@gmail.com>
2026-09-08 21:52:37 +0530

    changing local url to render

--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000"
+     //baseURL: "http://127.0.0.1:8000"
+     baseURL: "https://confluence-ai-backend.onrender.com"
 });
 
 // ---------------- Confluence APIs ----------------
